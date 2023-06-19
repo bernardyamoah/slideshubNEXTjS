ae1e919f0f2b1ab8cede54949109ad382bdaab79
Bernard Yaw Amoah Yamoah <bernardyamoah2016@gmail.com>
2023-06-19 15:35:50 -0700

    update

--- app/campus/page.tsx
+++ app/campus/page.tsx
@@ -3,11 +3,12 @@ import { useState, useEffect } from 'react';
 import { getCampus } from '@/lib/functions';
 import { Suspense } from 'react';
 import Loading from '../../components/ui/Cloading';
-import Image from 'next/image';
+
 import Link from 'next/link';
-import { Chip } from "@material-tailwind/react";
+
 import toast, { Toaster } from 'react-hot-toast';
-import {Card,CardHeader,CardBody,Typography,} from "@material-tailwind/react";
+
+import CampusCard from '@/components/CampusCard';
 
 
 interface Campus {
@@ -15,25 +16,28 @@ interface Campus {
   name: string;
   image: string;
   location: string;
+  $createdAt: string;
 }
 
 export default function CampusList() {
   const [campuses, setCampuses] = useState<Campus[]>([]);
-
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     async function fetchCampuses() {
       try {
-  
+
         const response = await toast.promise(getCampus(),
-        {
-          loading: 'fetctching campuses from database...',
-          success: <b>Campuses fetched!</b>,
-          error: <b>Could not load campuses.</b>,
-        });
-      
+          {
+            loading: 'Fetctching campus from database...',
+            success: <b>Campuses fetched!</b>,
+            error: <b>Could not load campuses.</b>,
+          });
+
         setCampuses(response);
+        setIsLoading(false);
       } catch (error) {
         console.error('Error fetching campuses:', error);
+        setIsLoading(false);
       }
     }
 
@@ -42,71 +46,58 @@ export default function CampusList() {
 
   return (
     <>
-    {/*
+      {/*
   Heads up! 👋
 
   This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
 */}
 
-<div
-  className="overflow-hidden bg-[url('https://images.unsplash.com/photo-1562157873-818bc0726f68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=654&q=80')] bg-cover bg-top bg-no-repeat"
->
-  <div className="bg-black/60 p-8 md:p-12 lg:px-16 lg:py-24">
-    <div className="text-center ltr:sm:text-left rtl:sm:text-right">
-      <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl">
-      Campus
-      </h2>
-
-      <p
-        className="hidden max-w-lg text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed text-center mx-auto"
+      <div
+        className="overflow-hidden bg-[url('https://images.unsplash.com/photo-1562157873-818bc0726f68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=654&q=80')] bg-cover bg-top bg-no-repeat"
       >
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
-        officia corporis quasi doloribus iure architecto quae voluptatum beatae
-        excepturi dolores.
-      </p>
+        <div className="bg-black/60 p-8 md:p-12 lg:px-16 lg:py-24">
+          <div className="text-center ltr:sm:text-left rtl:sm:text-right">
+            <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl">
+              Campus
+            </h2>
 
-    
-    </div>
-  </div>
-</div>
+            <p
+              className="hidden max-w-lg text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed text-center mx-auto"
+            >
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+              officia corporis quasi doloribus iure architecto quae voluptatum beatae
+              excepturi dolores.
+            </p>
+
+
+          </div>
+        </div>
+      </div>
       <section className="heading-link">
-    
+
         <p>
           <Link href="/">home</Link> / <Link href={`/campus/`}>Campus</Link>
         </p>
       </section>
-      <section className="container grid sm:grid-cols-2 lg:grid-cols-3 place-items-center gap-10 pb-10 ">
-        <Suspense fallback={<Loading/>}>
-          {campuses.map((campus) => (
-            <Card key={campus.$id}
-              className="max-w-xs transition-all hover:border-emerald-500 dark:hover:border-emerald-500 border-gray-200 w-full dark:bg-transparent group duration-300"
-            >
-              <Link href={{ pathname: `/campus/${campus.$id}/programs`, query: { campusId: campus.$id, name: campus.name, loc:campus.location } }} shallow passHref
+      <div className=" relative mx-auto flex flex-col items-center pb-10 px-2">
+        <div id="myUL">
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <ul className="md:container max-w-4xl grid sm:grid-cols-2 md:grid-cols-3 gap-8 pb-10">
+              <Suspense fallback={<Loading />}>
+                {campuses.map((campus) => (
+                  <CampusCard key={campus.$id} campusId={campus.$id} {...campus} timePosted={campus.$createdAt} />
+                ))}
+              </Suspense>
 
-              className="transition  group">
-                <CardHeader color="blue-gray" className="relative h-36 aspect-auto">
-                  <Image
-                    fill
-                    alt={campus.name}
-                    src={campus.image}
-                    className="transition-transform duration-500 ease-in-out object-center object-cover group-hover:scale-105"
-                  />
-                </CardHeader>
-                <CardBody>
-                  <Typography variant="h6" color="blue-gray">
-                    {campus.name}
-                  </Typography>
-                  <Typography className="font-normal text-left absolute bottom-0 right-0">
-                    <Chip className="w-fit" size="sm" value={campus.location.toUpperCase()} variant="gradient" />
-                  </Typography>
-                </CardBody>
-              </Link>
-            </Card>
-          ))}
-        </Suspense>
-        <Toaster />
-      
-      </section>
+
+            </ul>
+          )}
+        </div>
+      </div>
+      <Toaster />
+
     </>
   );
 }
--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -1,7 +1,7 @@
 import { formatTime } from '@/lib/functions';
 import Image from 'next/image'
 import Link from 'next/link';
-
+import {BookmarkSimple } from "@phosphor-icons/react";
 interface CourseCardProps {
   name: string;
   image: string;
@@ -35,28 +35,24 @@ const CourseCard: React.FC<CourseCardProps> = ({
     shallow
     passHref
     
-   className="rounded-xl border-2 border-gray-200 bg-white">
+   className="rounded-xl border-2 border-gray-200 bg-white max-w-xs">
       <div className="flex items-start gap-4 p-4 sm:p-6 lg:p-8">
+        {/* Course Image */}
         <Link
               href={{
                 pathname: `/campus/${campusId}/programs/${programId}/course/${courseId}`,
-                query: { courseId: courseId, name: name }
+                query: { courseId: courseId, name:name }
               }}
               shallow
               passHref
             
                className="block shrink-0">
-          <Image
-            alt=  {name}
-            src={image}
-            width={300}
-            height={200}
-            className="h-14 w-14 rounded-lg object-cover"
-          />
+          <BookmarkSimple size={30} weight="fill" />
         </Link>
-
+{/* Course Name */}
         <div>
-          <h3 className="font-medium sm:text-lg">
+          
+          <h3>
             <Link
               href={{
                 pathname: `/campus/${campusId}/programs/${programId}/course/${courseId}`,
@@ -64,13 +60,14 @@ const CourseCard: React.FC<CourseCardProps> = ({
               }}
               shallow
               passHref
-              className=""
+              className="font-medium sm:text-lg"
             >
               {name}
             </Link>
           </h3>
 
           <div className="mt-2 sm:flex sm:items-center sm:gap-2">
+            {/* Semester */}
             <div className="flex items-center gap-1 text-gray-500">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -88,43 +85,10 @@ const CourseCard: React.FC<CourseCardProps> = ({
               </svg>
               <p className="text-xs capitalize">{semester}</p>
             </div>
-            <div className="flex items-center gap-1 text-gray-500">
+
+            {/* Credit Hours */}
+            <div className="mt-1 sm:mt-0 flex items-center gap-1 text-gray-500">
             <svg
-  xmlns="http://www.w3.org/2000/svg"
-  className="h-4 w-4"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="currentColor"
-  strokeWidth="2"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
-  />
-</svg>
-
-              <p className="text-xs capitalize">{credit} credit hours</p>
-            </div>
-              
-
-            <span className="hidden sm:block" aria-hidden="true">&middot;</span>
-
-            <p className="hidden  sm:text-xs sm:text-gray-500">
-              Posted by{' '}
-              <a href="#" className="font-medium underline hover:text-gray-700">
-                John
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-    
-      <div className="flex justify-between">
-      <strong
-          className="-mb-[2px] -me-[2px] inline-flex items-center gap-1  px-3 py-1.5 text-gray-400"
-        >
-          <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-4 w-4"
             fill="none"
@@ -139,8 +103,28 @@ const CourseCard: React.FC<CourseCardProps> = ({
             />
           </svg>
 
+              <p className="text-xs capitalize">{credit} credit hours</p>
+            </div>
+              
+
+          
+
+          
+          </div>
+        </div>
+      </div>
+    
+      <div className="flex justify-between">
+        {/* Time Created */}
+        <strong
+          className="-mb-[2px] -me-[2px] inline-flex items-center gap-1  px-3 py-1.5 text-gray-400"
+        >
+          
+
           <span className="text-[10px] font-medium sm:text-xs">{formattedTime}</span>
         </strong>
+
+        {/* Course Code */}
         <strong
           className="-mb-[2px] -me-[2px] inline-flex items-center gap-1 rounded-ee-xl rounded-ss-xl bg-emerald-600 px-3 py-1.5 text-white"
         >
