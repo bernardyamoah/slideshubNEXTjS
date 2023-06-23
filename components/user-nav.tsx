4f8be266ac4aba35c2b6d6183a4b872c0dbc3eb8
Bernard Yaw Amoah Yamoah <bernardyamoah2016@gmail.com>
2023-06-23 14:26:10 +0000

    update

--- components/user-nav.tsx
+++ components/user-nav.tsx
@@ -16,22 +16,27 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import { logOut } from "@/lib/functions";
 
-export function UserNav() {
+import { useRouter } from 'next/navigation';
+;
+export const UserNav: React.FC<UserNavProps> = ({ user }) =>  {
+  const firstName = user?.name?.split(' ')[0] || '';
+  const router = useRouter();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
+            <AvatarImage src="/avatars/01.png" alt="{user?.name?.split(' ')[0] || ' '}" />
+            <AvatarFallback>{firstName.charAt(0).toLocaleUpperCase()}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-sm font-medium leading-none">{user?.name?.split(' ')[0] || ' '}</p>
             <p className="text-xs leading-none text-muted-foreground">
               m@example.com
             </p>
@@ -60,7 +65,7 @@ export function UserNav() {
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={() => logOut(router)}>
           <LogOut className="mr-2 h-4 w-4" />
           <span>Log out</span>
           <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
