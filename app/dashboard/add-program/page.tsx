0a34afcedd98a68cbf2a69ee1474160e47436fb4
Bernard Yaw Amoah Yamoah <bernardyamoah2016@gmail.com>
2023-06-25 01:54:17 +0000

    update

--- app/dashboard/add-program/page.tsx
+++ app/dashboard/add-program/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Metadata } from "next"
 
 import AddProgram from '@/components/AddProgram'
-export const metadata: Metadata = {
+ const metadata: Metadata = {
   title: "Add Slides",
   description: "Advanced form example using react-hook-form and Zod.",
 }
