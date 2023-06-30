1e41a2dff0f0e87795fe8a15e8a8223488f23637
Bernard Yaw Amoah Yamoah <bernardyamoah2016@gmail.com>
2023-06-30 11:53:37 +0000

    Change button text to "Sign Up" in UserAuthForm component

--- app/(clients)/(auth)/components/user-register.tsx
+++ app/(clients)/(auth)/components/user-register.tsx
@@ -136,7 +136,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
             {isLoading && (
               <Icons.spinner className="mr-2 h-4 w-4  animate-spin" />
             )}
-            Continue
+          Sign Up
           </Button>
         </div>
       </form>
