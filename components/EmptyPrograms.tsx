055bdd099eb684fcb7af71fea2e7238444d8be85
Bernard Yaw Amoah Yamoah <bernardyamoah2016@gmail.com>
2023-07-22 11:01:36 +0000

    update

--- components/EmptyPrograms.tsx
+++ components/EmptyPrograms.tsx
@@ -1,5 +1,5 @@
-'use client'
-import Link from "next/link";
+
+
 import Image from "next/image";
 import noProgram from '@/public/undraw_no_data_re_kwbl.svg'
 
@@ -8,7 +8,7 @@ export default function EmptyProgram() {
 	return (
 
 		<div className='w-full  flex flex-col items-center justify-center p-4'>
-			<Image src={noProgram} alt='Create an event' width={300} height={300} />
+			<Image src={noProgram} alt='Create an event' width={200} height={300} />
 			<h1 className='mt-10 text-center text-base font-bold sm:text-2xl'>Oops! There are no Programs here!</h1>
 
 			{/* <Button>
