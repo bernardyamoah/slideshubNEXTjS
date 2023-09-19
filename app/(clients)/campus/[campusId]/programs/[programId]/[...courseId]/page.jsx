f9fcb8ffed3e45e7cc2904d86e10ac1bf7763002
Bernard Yaw Amoah Yamoah <bernardyamoah2016@gmail.com>
2023-09-19 16:34:35 +0000

    update

--- app/(clients)/campus/[campusId]/programs/[programId]/[...courseId]/page.jsx
+++ app/(clients)/campus/[campusId]/programs/[programId]/[...courseId]/page.jsx
@@ -2,12 +2,7 @@
 
 import SlidesCard from '@/components/SlidesCard';
 export default async function FilesList({params:{courseId}}) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/courses/${courseId}`
-  );
-  const data = await response.json();
-  console.log("🚀 ~ file: page.jsx:9 ~ FilesList ~ data:", data)
- 
+
 return (
     <>
 
