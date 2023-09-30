f9bfd113a3a0aee2175dda482e502d6e1fa2ff91
Bernard Yaw Amoah Yamoah <bernardyamoah2016@gmail.com>
2023-09-30 12:01:43 +0000

    update

--- app/dashboard/_components/course-edit.tsx
+++ app/dashboard/_components/course-edit.tsx
@@ -23,6 +23,7 @@ import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, D
 import { Input } from "../../../components/ui/input"
 import { useState } from "react";
 import { Label } from "../../../components/ui/label";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { Popover, PopoverContent, PopoverTrigger } from "../../../components/ui/popover";
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "../../../components/ui/command";
@@ -86,69 +87,56 @@ export function CourseEdit({course, courses, setCourses }:CourseCardProps) {
   const [open1, setOpen1] = useState(false);
   const [open2, setOpen2] = useState(false);
   const [open3, setOpen3] = useState(false);
-  const [semester, setSemester] = useState("");
-  const [year, setYear] = useState("");
-  const [updatedName, setupdatedName] = useState("");
-  const [updatedLecturer, setupdatedLecturer] = useState("");
-  const [updatedCourseCode, setupdatedCourseCode] = useState("");
-  const [updatedYear, setupdatedYear] = useState("");
-  const [updatedCredit, setupdatedCredit] = useState("");
-  const [updatedSemester, setupdatedSemester] = useState("");
+
+  const [updatedName, setupdatedName] = useState(course.name);
+  const [updatedLecturer, setupdatedLecturer] = useState(course.lecturer);
+  const [updatedCourseCode, setupdatedCourseCode] = useState(course.courseCode);
+  const [updatedYear, setupdatedYear] = useState(course.year);
+  const [updatedCredit, setupdatedCredit] = useState(course.credit);
+  const [updatedSemester, setupdatedSemester] = useState(course.semester);
   const [showDialog, setShowDialog] = useState(false);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     
     try {
-      // year = year.charAt(0).toUpperCase() + year.slice(1);
-
-      // Perform the update
       const updatedAttributes: { 
         name?: string; 
         lecturer?: string; 
         courseCode?: string; 
         semester?: string; 
-        courseCredit?: string;
+        credit?: string;
         year?: string;
-        // Add other fields as needed
-      } = {};
-
-      const fieldsToUpdate = [
-        { field: 'name', value: updatedName },
-        { field: 'lecturer', value: updatedLecturer },
-        { field: 'courseCode', value: updatedCourseCode },
-        { field: 'semester', value: semester },
-        { field: 'year', value: year },
-        // Add other fields as needed
-      ];
-
-     
+      } = {
+        name: updatedName,
+        lecturer: updatedLecturer,
+        courseCode: updatedCourseCode,
+        semester: updatedSemester,
+        credit: updatedCredit,
+        year: updatedYear,
+      };
+  
       await updateCourse(course.$id, updatedAttributes);
-
-      
-    // Reset form fields
-    setupdatedName("");
-    setupdatedLecturer("");
-    setupdatedCourseCode("");
-    setSemester("");
-    setYear("");
-    // Reset other fields as needed
-
-    // Close the dialog
-    setOpen(false);
-
-    // Update the course list on the dashboard
-    const updatedCourses = courses.map((c) => {
-      if (c.$id === course.$id) {
-        return { ...c, ...updatedAttributes };
-      }
-      return c;
-    });
-    setCourses(updatedCourses);
-  } catch (error) {
-    console.error("Error updating course:", error);
-  }
-};
+  
+      setupdatedName("");
+      setupdatedLecturer("");
+      setupdatedCourseCode("");
+      setupdatedSemester("");
+      setupdatedYear("");
+  
+      setOpen(false);
+  
+      const updatedCourses = courses.map((c) => {
+        if (c.$id === course.$id) {
+          return { ...c, ...updatedAttributes };
+        }
+        return c;
+      });
+      setCourses(updatedCourses);
+    } catch (error) {
+      console.error("Error updating course:", error);
+    }
+  };
   const handleDeleteCourse = () => {
     deleteCourse(courseId);
     setShowDeleteDialog(false);
@@ -157,10 +145,13 @@ setCourses(updatedCourse);
   };
 
   const handleSemesterChange = (selectedValue: string) => {
-    setSemester(selectedValue);
+    setupdatedSemester(selectedValue);
   };
   const handleYearChange = (selectedValue: string) => {
-    setYear(selectedValue);
+    setupdatedYear(selectedValue);
+  };
+  const handleCreditChange = (selectedValue: string) => {
+    setupdatedCredit(selectedValue);
   };
   const [showDeleteDialog, setShowDeleteDialog] =useState(false)
 
@@ -168,14 +159,14 @@ setCourses(updatedCourse);
     <>
       <DropdownMenu >
         <DropdownMenuTrigger asChild>
-          <Button className="border-none p-2 h-2  bg-transparent text-gray-700 dark:text-gray-100 hover:bg-transparent">
+          <Button className="h-2 p-2 text-gray-700 bg-transparent border-none dark:text-gray-100 hover:bg-transparent">
             <span className="sr-only ">Actions</span>
-            <MoreHorizontal className="h-4 w-4" />
+            <MoreHorizontal className="w-4 h-4" />
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
         <DropdownMenuItem onSelect={() => setShowDialog(true)}>
-      <Edit className="mr-2 h-4 w-4" />
+      <Edit className="w-4 h-4 mr-2" />
       Edit 
     </DropdownMenuItem>
           <DropdownMenuSeparator />
@@ -183,60 +174,57 @@ setCourses(updatedCourse);
             onSelect={() => setShowDeleteDialog(true)}
             className="!text-red-600 hover:!bg-red-200/10"
           >
-            <Trash className="mr-2 h-4 w-4" />
+            <Trash className="w-4 h-4 mr-2" />
             Delete Course
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
 
-      <Dialog open={showDialog} onOpenChange={setShowDialog} >
-        <DialogContent className="sm:max-w-[480px] ">
-          <DialogHeader>
-            <DialogTitle>Change name </DialogTitle>
-            <DialogDescription>
-              Make changes to your slides here. Click save when you&apos;re done.
-            </DialogDescription>
-          </DialogHeader>
+          <ScrollArea className="h-[500px] overflow-auto">
+      <Dialog open={showDialog} onOpenChange={setShowDialog}  >
+        <DialogContent className="sm:max-w-[500px] overflow-auto">
+          
           <div className="grid gap-4 p-4">
-            <form className="mt-8 mb-2 w-full" onSubmit={handleSubmit}>
-            <div className="mb-4 flex flex-col gap-3">
-                <Label htmlFor="name" className="text-left block col-span-4">
+            <form className="w-full mt-8 mb-2 space-y-4" onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-3 mb-4">
+                <Label htmlFor="name" className="block col-span-4 text-left">
                   Name
                 </Label>
                 <Input
                   id="name"
-                  value={course.name}
+                  value={updatedName}
                   onChange={(event) => setupdatedName(event.target.value)}
                 />
               </div>
 
-              {/* Credit hours */}
-              <div className="mb-4 flex flex-col gap-3">
-                <Label htmlFor="lecturer" className="text-left block col-span-4">
+              {/* Lecturer */}
+              <div className="flex flex-col gap-3 mb-4">
+                <Label htmlFor="lecturer" className="block col-span-4 text-left">
                   Lecturer
                 </Label>
                 <Input
                   id="lecturer"
-                  value={course.lecturer}
+                  value={updatedLecturer}
                   onChange={(event) => setupdatedLecturer(event.target.value)}
                 />
               </div>
 
-              {/* Course code */}
-              <div className="mb-4 flex flex-col gap-3">
-                <Label htmlFor="courseCode" className="text-left block col-span-4">
+        <div className="grid gap-4 mb-1.5 md:grid-cols-2">
+                {/* Course code */}
+                <div className="flex flex-col space-y-1.5 flex-1 w-full ">
+                <Label htmlFor="courseCode" className="block col-span-4 text-left">
                   Course Code
                 </Label>
                 <Input
                   id="courseCode"
-                  value={course.courseCode}
+                  value={updatedCourseCode}
                   onChange={(event) => setupdatedCourseCode(event.target.value)}
                 />
               </div>
 
 
                 {/* Year */}
-                <div className="flex flex-col space-y-1.5 flex-1 w-full">
+                <div className="flex flex-col space-y-1.5 flex-1 w-full ">
                 <Label htmlFor="year">Year</Label>
                 <Popover open={open2} onOpenChange={setOpen2}>
                   <PopoverTrigger asChild>
@@ -244,16 +232,16 @@ setCourses(updatedCourse);
                       variant="outline"
                       role="combobox"
                       aria-expanded={open2}
-                      className="w-full justify-between  overflow-hidden no-wrap"
+                      className="justify-between w-full overflow-hidden no-wrap"
                     >
-                      {year
-                        ? Years.find((level) => level.id === year)?.level
+                      {updatedYear
+                        ? Years.find((level) => level.id === updatedYear)?.level
                         : "Select Level"}
-                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
                     </Button>
                   </PopoverTrigger>
                   <PopoverContent className="w-[200px] p-0">
-                    <Command onValueChange={handleYearChange}>
+                    <Command onValueChange={handleYearChange} >
                       <CommandInput placeholder="Search ..." />
                       <CommandEmpty>No year found</CommandEmpty>
                       <CommandGroup>
@@ -261,17 +249,66 @@ setCourses(updatedCourse);
                           <CommandItem
                             key={level.id}
                             onSelect={(currentValue) => {
-                              setYear(currentValue.toUpperCase() === year ? "" : currentValue);
+                              setupdatedYear(currentValue.toUpperCase() === updatedYear ? "" : currentValue);
                               setOpen2(false); // Updated from setOpen2(false)
                             }}
                           >
                             <Check
                               className={cn(
                                 "mr-2 h-4 w-4",
-                                year === level.id ? "opacity-100" : "opacity-0"
+                                updatedYear === level.id ? "opacity-100" : "opacity-0"
                               )}
                             />
                             {level.level}
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+                </div>
+
+        </div>
+
+                <div className="grid gap-4 mb-1.5 md:grid-cols-2">
+                {/* Credit Hours */}
+
+                <div className="flex flex-col space-y-1.5 flex-1 w-full">
+                <Label htmlFor="credit">Credit</Label>
+                <Popover open={open1} onOpenChange={setOpen1}>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant="outline"
+                      role="combobox"
+                      aria-expanded={open1}
+                      className="justify-between w-full overflow-hidden no-wrap"
+                    >
+                      {updatedCredit
+                        ? creditHours.find((credit) => credit.id === updatedCredit)?.hour
+                        : "Select Level"}
+                      <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-[200px] p-0">
+                    <Command onValueChange={handleCreditChange} >
+                      <CommandInput placeholder="Search ..." />
+                      <CommandEmpty>No year found</CommandEmpty>
+                      <CommandGroup>
+                        {creditHours.map((credit) => (
+                          <CommandItem
+                            key={credit.id}
+                            onSelect={(currentValue) => {
+                              setupdatedCredit(currentValue.toUpperCase() === updatedCredit ? "" : currentValue);
+                              setOpen2(false); // Updated from setOpen2(false)
+                            }}
+                          >
+                            <Check
+                              className={cn(
+                                "mr-2 h-4 w-4",
+                                updatedCredit === credit.id ? "opacity-100" : "opacity-0"
+                              )}
+                            />
+                            {credit.hour}
                           </CommandItem>
                         ))}
                       </CommandGroup>
@@ -290,12 +327,12 @@ setCourses(updatedCourse);
                       variant="outline"
                       role="combobox"
                       aria-expanded={open2}
-                      className="w-full justify-between  overflow-hidden no-wrap"
+                      className="justify-between w-full overflow-hidden no-wrap"
                     >
-                      {semester
-                        ? Semesters.find((sem) => sem.id === semester)?.semester
+                      {updatedSemester
+                        ? Semesters.find((sem) => sem.id === updatedSemester)?.semester
                         : "Select Semester"}
-                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
                     </Button>
                   </PopoverTrigger>
                   <PopoverContent className="w-[200px] p-0">
@@ -307,14 +344,14 @@ setCourses(updatedCourse);
                           <CommandItem
                             key={sem.id}
                             onSelect={(currentValue) => {
-                              setSemester(currentValue.toUpperCase() === semester ? "" : currentValue);
+                              setupdatedSemester(currentValue.toUpperCase() === updatedSemester ? "" : currentValue);
                               setOpen2(false); // Updated from setOpen2(false)
                             }}
                           >
                             <Check
                               className={cn(
                                 "mr-2 h-4 w-4",
-                                semester === sem.id ? "opacity-100" : "opacity-0"
+                                updatedSemester === sem.id ? "opacity-100" : "opacity-0"
                               )}
                             />
                             {sem.semester}
@@ -324,14 +361,17 @@ setCourses(updatedCourse);
                     </Command>
                   </PopoverContent>
                 </Popover>
-              </div>
+               </div>
+                </div>
+               
 
               
 
-              {/* Add similar input fields for other properties */}
+              
             
             </form>
           </div>
+         
           <DialogFooter>
             <div className="hidden sm:block">
               <Button variant="secondary" onClick={() => setShowDialog(false)}>
@@ -344,6 +384,7 @@ setCourses(updatedCourse);
           </DialogFooter>
         </DialogContent>
       </Dialog>
+          </ScrollArea>
 
       <AlertDialog open={showDeleteDialog} onOpenChange={setShowDeleteDialog}>
         <AlertDialogContent>
