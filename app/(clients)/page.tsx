0776e658b723b1ceecd8f0da5b33f5e55c0bbea8
Bernard Yaw Amoah Yamoah <bernardyamoah2016@gmail.com>
2023-10-04 18:19:57 +0000

    ✨Added Testimonials component 🗯️:

--- app/(clients)/page.tsx
+++ app/(clients)/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import Head from "next/head";
+
 import Features from "@/components/Features";
 import Testimonials from "@/components/Testimonials";
 import Hero from "@/components/Hero";
@@ -8,29 +8,15 @@ export default function Home() {
 	return (
 		<>
 
-			<Head>
-				<meta
-					property="twitter:image"
-					content="https://slideshub.netlify.app/assets/favicon_io/thumbnail.jpg"
-				/>
 
-				<meta name="description" content="" />
-				{/* <!--Your description--> */}
-				<meta name="author" content="Bernard Yamoah" />
+				
 
-				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<meta name="twitter:title" content="Your Page Title" />
-				<meta name="twitter:description" content="Description of your page" />
-
-				<title>SlidesHub</title>
-			</Head>
-
-			<main>
-				{/* <!-- Hero section --> */}
+			<main >
+			<div className="bg-pattern opacity-5 dark:opacity-25 w-full h-full absolute bg-repeat bg-center  -z-10"></div>
+				{/* <-- Hero section --> */}
 				<Hero />
 
-				{/* <!-- Features Section --> */}
+				{/* <-- Features Section --> */}
 				<Features />
 
 				{/*Testimonial Section */}
--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -11,7 +11,7 @@ export default function Testimonials() {
 		<>
 			<section className=" py-10   mx-auto w-full">
 				<div className="w-full">
-					<p className="text-sm md:text-base font-medium text-emerald-500 md:text-center">Testimonials</p>
+					<p className="text-lg font-medium text-emerald-500 text-center">Testimonials</p>
 
 					<h1 className="mt-2 text-3xl font-semibold capitalize text-gray-800 dark:text-white lg:text-4xl md:text-center">
 						What users are saying
@@ -19,40 +19,40 @@ export default function Testimonials() {
 
 					<main className="relative z-[2] mt-8 w-full md:flex md:items-center xl:mt-12 mx-auto">
 						<div className="absolute -z-10 w-full rounded-2xl  md:h-72"></div>
-						<div className=" mySwiper w-full rounded-2xl  shadow-lg md:flex md:items-center md:justify-evenly md:bg-none md:p-0 md:shadow-none lg:px-12 ">
-							<div  >
+						<div className=" mySwiper w-full rounded-2xl   md:flex md:items-center md:justify-evenly md:bg-none md:p-0 md:shadow-none lg:px-12 ">
+							<div >
 
 
 								{/* First slide*/}
 								<aside>
-									<div className="md:p-6 md:flex md:items-center md:space-x-10">
-										<div className="md:w-1/2">
+									<div className=" md:flex  md:space-x-10 ">
+										<div className="w-1/2 ">
 											<Image
-												className="aspect-square h-2/6 w-2/6 rounded-full object-cover shadow-md md:!h-[32rem] md:!w-full md:rounded-2xl lg:h-[36rem] lg:w-[26rem]"
+												className=" h-1/2 w-2/3 rounded-full object-cover shadow-md md:!h-[22rem] md:!w-full md:rounded-2xl  lg:w-[26rem]"
 												src="https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
 												alt="client photo"
-												width={300} height={300}
+												width={500} height={500}
 											/>
 										</div>
 
-										<Card className="mt-3 p-4">
-											<CardDescription>
+										<blockquote className="border rounded-lg bg-card mt-3 p-4 md:max-w-sm self-start">
+											
 											<CardTitle className="mb-2 text-2xl" >
-												Bernard
+												Vicentia Agyei
 											</CardTitle>
-											<Badge variant='secondary' className=" ">
+											<Badge variant='outline' className="rounded-3xl text-emerald-500 ">
 												BSc Materials Engineering, KNUST
 											</Badge>
-											</CardDescription>
+										
 
-											<div className="mt-2">
+											<figcaption className="mt-3 text-sm">
 
 												“Lorem ipsum dolor sit amet, consectetur adipisicing
 												elit. Tempore quibusdam ducimus libero ad tempora
 												doloribus expedita laborum saepe voluptas perferendis
 												delectus assumenda”.
-											</div>
-										</Card>
+											</figcaption>
+										</blockquote>
 									</div>
 								</aside>
 							</div>
