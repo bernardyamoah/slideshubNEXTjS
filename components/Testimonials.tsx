83e678eb9ceeb1b4ec8bdb536d0d1c5b6daa6d11
Bernard Yaw Amoah Yamoah <bernardyamoah2016@gmail.com>
2023-10-04 18:46:47 +0000

    update

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -13,29 +13,29 @@ export default function Testimonials() {
 				<div className="w-full">
 					<p className="text-lg font-medium text-emerald-500 text-center">Testimonials</p>
 
-					<h1 className="mt-2 text-3xl font-semibold capitalize text-gray-800 dark:text-white lg:text-4xl md:text-center">
+					<h1 className="mt-2 text-3xl font-semibold capitalize text-gray-800 dark:text-white lg:text-4xl text-center">
 						What users are saying
 					</h1>
 
 					<main className="relative z-[2] mt-8 w-full md:flex md:items-center xl:mt-12 mx-auto">
 						<div className="absolute -z-10 w-full rounded-2xl  md:h-72"></div>
 						<div className=" mySwiper w-full rounded-2xl   md:flex md:items-center md:justify-evenly md:bg-none md:p-0 md:shadow-none lg:px-12 ">
-							<div >
+							<div className="">
 
 
 								{/* First slide*/}
-								<aside>
-									<div className=" md:flex  md:space-x-10 ">
+								<aside className=" ">
+									<div className="md:flex  md:space-x-10 ">
 										<div className="w-1/2 ">
 											<Image
-												className=" h-1/2 w-2/3 rounded-full object-cover shadow-md md:!h-[22rem] md:!w-full md:rounded-2xl  lg:w-[26rem]"
+												className=" h-24 w-24 rounded-full object-cover shadow-md md:!h-[22rem] md:!w-full md:rounded-2xl  lg:w-[26rem]"
 												src="https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
 												alt="client photo"
 												width={500} height={500}
 											/>
 										</div>
 
-										<blockquote className="border rounded-lg bg-card mt-3 p-4 md:max-w-sm self-start">
+										<blockquote className="md:border rounded-lg bg-card mt-3 p-4 md:max-w-sm self-start">
 											
 											<CardTitle className="mb-2 text-2xl" >
 												Vicentia Agyei
@@ -45,7 +45,7 @@ export default function Testimonials() {
 											</Badge>
 										
 
-											<figcaption className="mt-3 text-sm">
+											<figcaption className="mt-3 text-xs md:text-sm">
 
 												“Lorem ipsum dolor sit amet, consectetur adipisicing
 												elit. Tempore quibusdam ducimus libero ad tempora
