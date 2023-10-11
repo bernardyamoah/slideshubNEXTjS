40ba348732ead2b298e7820059559c7ef2edeb1b
Bernard Yaw Amoah Yamoah <bernardyamoah2016@gmail.com>
2023-10-11 08:06:38 +0000

    test update

--- app/(clients)/books/page.tsx
+++ app/(clients)/books/page.tsx
@@ -11,9 +11,9 @@ export default function page() {
   return (
   // <EmptyBooks/>
   <>
-   {/* return <DocViewer   pluginRenderers={[PDFRenderer, PNGRenderer]} documents={docs}  />; */}
-
-  <div className='max-w-4xl mx-auto'> <iframe name='file' className='w-full h-screen'  src="https://cloud.appwrite.io/v1/storage/buckets/6489a0a6b548f2079126/files/64d0aaaef070714e2e32/view?project=647a8fc613adfa0667f9&mode=admin" ></iframe></div>
+   return <DocViewer   pluginRenderers={[PDFRenderer, PNGRenderer]} documents={docs}  />;
+{/* 
+  <div className='max-w-4xl mx-auto'> <iframe name='file' className='w-full h-screen'  src="https://cloud.appwrite.io/v1/storage/buckets/6489a0a6b548f2079126/files/64d0aaaef070714e2e32/view?project=647a8fc613adfa0667f9&mode=admin" ></iframe></div> */}
   </>
   )
 }
