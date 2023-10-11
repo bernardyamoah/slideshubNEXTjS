1b26aa8e0de07061cd24f8bca5e2e4343dcbe200
Bernard Yaw Amoah Yamoah <bernardyamoah2016@gmail.com>
2023-10-11 08:42:01 +0000

    text -update

--- app/(clients)/books/page.tsx
+++ app/(clients)/books/page.tsx
@@ -1,19 +1,39 @@
 'use client'
+import Loading from '@/components/ui/Cloading';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
 // import EmptyBooks from '@/components/EmptyBooks'
 import React, { useState } from 'react'
-import DocViewer, {  PDFRenderer, PNGRenderer  } from "@cyntler/react-doc-viewer";
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc=new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString()
+import {Document,Page} from 'react-pdf'
+
 export default function page() {
-  const docs = [
-    { uri: "https://cloud.appwrite.io/v1/storage/buckets/6489a0a6b548f2079126/files/64d0aaaef070714e2e32/view?project=647a8fc613adfa0667f9&mode=admin" }
-    
-  ];
-  
+  const [numPages, setNumPages]=useState<number>()
+  const [pageNumber, setPageNumber]=useState<number>(1)
+  function onDocumentLoadSuccess({numPages}:{numPages:number}):void{
+    setNumPages(numPages)
+
+  }
+  const handleNextPage = () => {
+    if (numPages && pageNumber < numPages) {
+      setPageNumber(pageNumber + 1);
+    }
+  }
   return (
-  // <EmptyBooks/>
-  <>
-   return <DocViewer   pluginRenderers={[PDFRenderer, PNGRenderer]} documents={docs}  />;
-{/* 
-  <div className='max-w-4xl mx-auto'> <iframe name='file' className='w-full h-screen'  src="https://cloud.appwrite.io/v1/storage/buckets/6489a0a6b548f2079126/files/64d0aaaef070714e2e32/view?project=647a8fc613adfa0667f9&mode=admin" ></iframe></div> */}
-  </>
+<>
+<Button onClick={handleNextPage}>Next</Button>
+<div className=' mx-auto border overflow-hidden'>
+  <Document className='max-w-2xl mx-auto h-screen overflow-scroll-y' file={'https://cloud.appwrite.io/v1/storage/buckets/6489a0a6b548f2079126/files/64d0aaaef070714e2e32/view?project=647a8fc613adfa0667f9&mode=admin'} onLoadSuccess={onDocumentLoadSuccess} loading={Loading}>
+    <Page  pageNumber={pageNumber}/>
+  </Document>
+ 
+  <Badge>Page {pageNumber
+  } of {numPages}</Badge>
+</div>
+</>
   )
 }
--- next.config.js
+++ next.config.js
@@ -12,6 +12,10 @@ const nextConfig = {
 		serverActions: true,
 		
 	},
+	webpack: (config)=>{
+		config.resolve.alias.canvas=false;
+		return config
+	}
 };
 
 module.exports = nextConfig;
