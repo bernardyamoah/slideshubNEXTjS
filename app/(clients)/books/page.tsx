1fa87fc6f15d71ca74f0bf120a7b58791f6a9e78
Bernard Yaw Amoah Yamoah <bernardyamoah2016@gmail.com>
2023-10-11 09:36:15 +0000

    update

--- app/(clients)/books/page.tsx
+++ app/(clients)/books/page.tsx
@@ -8,6 +8,7 @@ import 'react-pdf/dist/esm/Page/TextLayer.css';
 import './Sample.css';
 
 import type { PDFDocumentProxy } from 'pdfjs-dist';
+import { Button } from '@/components/ui/button';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
@@ -22,20 +23,29 @@ const options = {
 type PDFFile = string | File | null;
 
 export default function Sample() {
-  const [file, setFile] = useState<PDFFile>('https://cloud.appwrite.io/v1/storage/buckets/6489a0a6b548f2079126/files/64d0aaaef070714e2e32/view?project=647a8fc613adfa0667f9&mode=admin');
-  const [numPages, setNumPages] = useState<number>();
+  const file = 'https://cloud.appwrite.io/v1/storage/buckets/6489a0a6b548f2079126/files/64d0aaaef070714e2e32/view?project=647a8fc613adfa0667f9&mode=admin';
 
-  function onFileChange(event: React.ChangeEvent<HTMLInputElement>): void {
-    const { files } = event.target;
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
-    if (files && files[0]) {
-      setFile(files[0] || null);
-    }
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
   }
 
-  function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void {
-    setNumPages(nextNumPages);
+  function changePage(offset) {
+    setPageNumber(prevPageNumber => prevPageNumber + offset);
   }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
+
+
 
   return (
     <div className="Example">
@@ -43,24 +53,36 @@ export default function Sample() {
         <h1>react-pdf sample page</h1>
       </header>
       <div className="Example__container">
-        <div className="Example__container__load">
-          <label htmlFor="file">Load from file:</label>{' '}
-          <input onChange={onFileChange} type="file" />
-        </div>
-        <div className="Example__container__document">
-          <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
-            {Array.from(new Array(numPages), (el, index) => (
-              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-            ))}
-          </Document>
-        </div>
+      
+        <Document
+        file={file}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <div>
+        <p>
+          Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+        </p>
+        <Button
+          type="button"
+          disabled={pageNumber <= 1}
+          onClick={previousPage}
+        >
+          Previous
+        </Button>
+        <Button
+          type="button"
+          disabled={pageNumber >= (numPages || 0)}
+          onClick={nextPage}
+        >
+          Next
+        </Button>
+      </div>
       </div>
     </div>
   );
 }
-
-
-
 
 
 
