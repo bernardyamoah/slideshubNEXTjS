694a6bd244d264209e39dd7a8bd0a76770c99d35
Bernard Yaw Amoah Yamoah <bernardyamoah2016@gmail.com>
2023-10-17 10:18:29 +0000

    update

---
+++ app/(clients)/(auth)/components/user-auth copy.tsx
@@ -0,0 +1,107 @@
+"use client"
+
+
+import { cn } from "@/lib/utils"
+import { Icons } from "@/components/icons"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useForm } from "react-hook-form";
+import { handleGoogleSignIn } from "@/lib/functions"
+import { Loader2 } from "lucide-react"
+import { useUserContext } from "@/components/UserContext"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useState } from "react"
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const loginSchema=z.object({
+  email:z.string().email(),
+  password:z.string().min(6)
+})
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+
+    },
+  });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const {login}=useUserContext();
+
+ 
+  async function onSubmit(data:z.infer<typeof loginSchema>) {
+    const {email,password}=data
+    login(email, password);
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="grid gap-2 space-y-4">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+           
+              autoComplete="email"
+              autoCorrect="off"
+              
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="password">
+              Password
+            </Label>
+            <Input
+              id="password"
+              placeholder="Password"
+              type="password"
+              autoCapitalize="none"
+              autoComplete="password"
+              autoCorrect="off"
+          
+              value={password}
+			 onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <Button disabled={form.formState.isSubmitting} className="mt-4">
+            {form.formState.isSubmitting && (
+              <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+              
+            )}
+            Login
+          </Button>
+        </div>
+      </form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="px-2 bg-background text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
+      <Button variant="outline" type="button" disabled={form.formState.isSubmitting} onClick={handleGoogleSignIn}>
+        {form.formState.isSubmitting ? (
+          <Icons.spinner className="w-4 h-4 mr-2 animate-spin" />
+        ) : (
+          <Icons.google className="w-4 h-4 mr-2" />
+        )}{" "}
+    Google
+      </Button>
+    </div>
+  )
+}
