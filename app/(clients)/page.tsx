e24c9ed4220b6e523471aa24ca827dbca56f1bb3
bernardyamoah <bernardyamoah2016@gmail.com>
2024-01-19 17:12:42 +0000

    fix hero import

--- app/(clients)/page.tsx
+++ app/(clients)/page.tsx
@@ -1,30 +1,19 @@
-'use client'
-
+"use client";
 import Features from "@/components/feature";
 import Hero from "@/components/hero";
 
-
 // import Testimonials from "@/components/Testimonials";
-
 
 export default function Home() {
 	return (
 		<>
+			{/* <-- Hero section --> */}
+			<Hero />
 
+			{/* <-- Features Section --> */}
+			<Features />
 
-				
-
-			
-			
-				{/* <-- Hero section --> */}
-				<Hero />
-
-				{/* <-- Features Section --> */}
-				<Features />
-
-				
-				{/* <Testimonials /> */}
-			
+			{/* <Testimonials /> */}
 		</>
 	);
 }
